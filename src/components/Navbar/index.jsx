9d8b98586d023dc259fd09ceeaeb1f35bf274c38
MHmolesini <Mhmolesini@gmail.com>
2023-03-05 19:40:08 -0300

    se agrega al navbar el icono del CartWidget

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,17 @@
 import React from "react"
 import css from "./navbar.module.css"
+
+import {CartWidget} from '../CartWidget/index'
 // import { shop } from ".../"
 
 export const Navbar = () => {
     return <div className={css.header__navbar}>
         <ul className={css.header__navbar__menu}>
             <li><a className={css.active}>Tienda</a></li>
-            <li><a href="./contacto.html">Contactanos</a></li>
+            <li><a>Contactanos</a></li>
             <li><a>Sobre nosotros</a></li>
             <li><a>Preguntas frecuentes</a></li>
-            {/* <li><a><img src={shop}/></a></li> */}
+            <li><a className="header__navbar__menu__item"><CartWidget /></a></li>
         </ul>
 
     </div>
