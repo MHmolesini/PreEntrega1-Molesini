943e728d4c0870e0661a2f100c6793759b37be64
MHmolesini <Mhmolesini@gmail.com>
2023-03-05 17:17:09 -0300

    se agrego una lista de productos

---
+++ src/assets/shop/products.js
@@ -0,0 +1,56 @@
+// Aca importo las imagenes
+import product1 from '../images/products/1.png'
+import product2 from '../images/products/2.png'
+import product3 from '../images/products/3.png'
+import product4 from '../images/products/4.png'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Remera Rosa",
+        price: 1200.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Top negro",
+        price: 1000.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Top violeta",
+        price: 1000.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Remera blanca",
+        price: 1200.0,
+        productImage: product4,
+    },
+    {
+        id: 4,
+        productName: "Remera blanca",
+        price: 1200.0,
+        productImage: product4,
+    },
+    {
+        id: 4,
+        productName: "Remera blanca",
+        price: 1200.0,
+        productImage: product4,
+    },
+    {
+        id: 4,
+        productName: "Remera blanca",
+        price: 1200.0,
+        productImage: product4,
+    },
+    {
+        id: 4,
+        productName: "Remera blanca",
+        price: 1200.0,
+        productImage: product4,
+    },
+]
