9898257f1b197d1ef75eacbbf606c644d08dbc46
MHmolesini <Mhmolesini@gmail.com>
2023-03-05 17:20:33 -0300

    se modifica el jsx del navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import css from "./navbar.module.css"
 // import { shop } from ".../"
 
 export const Navbar = () => {
-    return <div className="header__navbar">
-        <ul className="header__navbar__menu">
-            <li><a className="active">Tienda</a></li>
+    return <div className={css.header__navbar}>
+        <ul className={css.header__navbar__menu}>
+            <li><a className={css.active}>Tienda</a></li>
             <li><a href="./contacto.html">Contactanos</a></li>
             <li><a>Sobre nosotros</a></li>
             <li><a>Preguntas frecuentes</a></li>
