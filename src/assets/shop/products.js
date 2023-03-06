60ffcd4f2e228e1bd6965c876f7e8767a08d334f
MHmolesini <Mhmolesini@gmail.com>
2023-03-05 21:40:48 -0300

    se incluyeron mas imagenes de prueba, se movio el archivo products.js a la carpeta shop, y se crearon 8 productos

--- src/assets/shop/products.js
+++ src/assets/shop/products.js
@@ -1,8 +1,12 @@
-// Aca importo las imagenes
+// importo las imagenes
 import product1 from '../images/products/1.png'
 import product2 from '../images/products/2.png'
 import product3 from '../images/products/3.png'
 import product4 from '../images/products/4.png'
+import product5 from '../images/products/5.png'
+import product6 from '../images/products/6.png'
+import product7 from '../images/products/7.png'
+import product8 from '../images/products/8.png'
 
 export const PRODUCTS = [
     {
@@ -30,27 +34,27 @@ export const PRODUCTS = [
         productImage: product4,
     },
     {
-        id: 4,
-        productName: "Remera blanca",
-        price: 1200.0,
-        productImage: product4,
+        id: 5,
+        productName: "Jean Azul",
+        price: 6000.0,
+        productImage: product5,
     },
     {
-        id: 4,
-        productName: "Remera blanca",
-        price: 1200.0,
-        productImage: product4,
+        id: 6,
+        productName: "Pantalon negro",
+        price: 6500.0,
+        productImage: product6,
     },
     {
-        id: 4,
-        productName: "Remera blanca",
-        price: 1200.0,
-        productImage: product4,
+        id: 7,
+        productName: "Biker negro",
+        price: 2200.0,
+        productImage: product7,
     },
     {
-        id: 4,
-        productName: "Remera blanca",
-        price: 1200.0,
-        productImage: product4,
+        id: 8,
+        productName: "Biker gris",
+        price: 2200.0,
+        productImage: product8,
     },
 ]
