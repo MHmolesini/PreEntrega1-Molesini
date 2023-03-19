f3529cde931e09c43a81e7d8a5c277c6199f3c47
MHmolesini <Mhmolesini@gmail.com>
2023-03-19 18:15:10 -0300

    Se diseñaron las cards en donde se va a mostrar el resumen de los productos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,17 @@
-const Card = ({ products }) => {
+import React from "react";
+import css from './card.module.css'
+
+export const Card = (card) => {
     return (
-        <div>
-            <img src={products.productImage}></img>
-            <p>{products.productName}</p>
+        <div className={css.card}>
+            <div className={css.card__img}>
+                <img src={card.productImage}></img>
+            </div>
+            <div className={css.card__description}>
+                <p className={css.card__description__price}>$ {card.price}</p>
+                <p>{card.productName}</p>
+                <button>Agregar</button>
+            </div>
         </div>
     )
 }
