eaf0619b294e84a6466dc0e476aa137ca4fca3ed
MHmolesini <Mhmolesini@gmail.com>
2023-03-19 21:08:21 -0300

    links a las cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import css from './card.module.css'
 
 export const Card = (card) => {
     return (
-        <div className={css.card}>
-            <div className={css.card__img}>
-                <img src={card.productImage}></img>
+        <Link to={`${card.id}`}>
+            <div className={css.card}>
+                <div className={css.card__img}>
+                    <img src={card.productImage}></img>
+                </div>
+                <div className={css.card__description}>
+                    <p className={css.card__description__price}>$ {card.price}</p>
+                    <p>{card.productName}</p>
+                    <p>{card.id}</p>
+                    <button>Agregar</button>
+                </div>
             </div>
-            <div className={css.card__description}>
-                <p className={css.card__description__price}>$ {card.price}</p>
-                <p>{card.productName}</p>
-                <button>Agregar</button>
-            </div>
-        </div>
+        </Link>
     )
 }
